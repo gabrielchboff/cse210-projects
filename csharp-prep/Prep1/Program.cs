d45dfcb24c9a8898b25bf9ee3128f7cc38f679c2
gabrielchboff <gabrielchagasboff@gmail.com>
2024-02-27 09:01:17 -0300

    fix: Name on the readme file

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
-using System;
+﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your name?");
+        string userName = Console.ReadLine();
+        Console.WriteLine($"Hello, {userName}!");
+        Console.WriteLine("What is your favorite number?");
+        int favNum = int.Parse(Console.ReadLine());
+        int favNumSquared = favNum * favNum;
+        Console.WriteLine($"Your favorite number squared is {favNumSquared}");
     }
 }
