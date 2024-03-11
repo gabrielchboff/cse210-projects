7559eda8fd1076196d175333736e2b91633c5f2a
gabrielchboff <gabrielchagasboff@gmail.com>
2024-03-11 11:03:52 -0300

    feat: Learn03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,22 @@
-using System;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.WriteLine("Learning03");
+        
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(1);
+        Fraction f3 = new Fraction(3, 4);
+        
+
+        Console.WriteLine("The first fraction is " + f1.GetFractionString());
+        Console.WriteLine("The second fraction is " + f2.GetFractionString());
+        Console.WriteLine("The third fraction is " + f3.GetFractionString());
+
+        Console.WriteLine("The first fraction is " + f1.GetDecimalValue());
+        Console.WriteLine("The second fraction is " + f2.GetDecimalValue());
+        Console.WriteLine("The third fraction is " + f3.GetDecimalValue());
+
     }
+
 }
